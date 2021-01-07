1d2b9607a5650aa7b042c24ca7657998d555636b
Morgan Miller <morganm.ewc@gmail.com>
2021-01-06 20:24:18 -0500

    updates to the highscore.js file

---
+++ models/highscore.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const highscoreSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  // url string for thumbnail image
+  thumbnail: {
+    type: String,
+    default: ""
+  },
+  // url for highscore web page - unique index
+  href: {
+    type: String,
+    default: "",
+    unique: true
+  },
+
+
+  highscore: [String]
+});
+
+const Highscore = mongoose.model("Highscore", highscoreSchema);
+
+module.exports = Highscore;
