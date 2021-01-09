29e29e041bb27249fe9b373acbc750a4f3d0a599
SusanBeno <beno.susan@gmail.com>
2021-01-09 13:36:15 -0500

    controllers and routers

---
+++ controllers/gameController.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+const { modelName } = require("../models/highscore");
+
+module.exports = {
+    listAll: function(req, res) {
+        db.Highscore
+        .find(req.body)
+        .then(scoreList => res.json(scoreList))
+        .catch(err => res.status(422).end())
+    },
+
+    create: function(req, res) {
+        db.Highscore
+        .create(req.body)
+        .then(scoreList => res.json(scoreList))
+        .catch(err => res.status(422).json(err));
+    },
+
+    update: function(req, res) {
+        db.Highscore
+        .newList(req.body)
+        .then(scoreList => res.json(scoreList))
+        .catch(err => res.status(422).json(err));
+    }
+
+}
+
+
+
