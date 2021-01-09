7a6eaacdbc0613c863a3babbe622a668f6ceda9f
Morgan Miller <morganm.ewc@gmail.com>
2021-01-09 12:45:39 -0500

    updating the memorygame.js file

--- models/highscore.js
+++ models/highscore.js
@@ -1,27 +1,27 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const highscoreSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  // url string for thumbnail image
-  thumbnail: {
-    type: String,
-    default: ""
-  },
-  // url for highscore web page - unique index
-  href: {
-    type: String,
-    default: "",
-    unique: true
-  },
+// const highscoreSchema = new mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true
+//   },
+//   // url string for thumbnail image
+//   thumbnail: {
+//     type: String,
+//     default: ""
+//   },
+//   // url for highscore web page - unique index
+//   href: {
+//     type: String,
+//     default: "",
+//     unique: true
+//   },
 
 
-  highscore: [String]
-});
+//   highscore: [String]
+// });
 
-const Highscore = mongoose.model("Highscore", highscoreSchema);
+// const Highscore = mongoose.model("Highscore", highscoreSchema);
 
-module.exports = Highscore;
+// module.exports = Highscore;
