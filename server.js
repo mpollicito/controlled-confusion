2cac2bf32a2c3bedabe878e1dc46ac6a152cb2fe
adt315 <angela_dyan@hotmail.com>
2021-01-09 23:25:04 -0500

    deleted files, added css and jsx link to index.html

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 app.post("/submit", ({ body }, res) => {
   User.create(body)
