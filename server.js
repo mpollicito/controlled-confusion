4b871f6f896631aeb6b3661da8c8b40b585f0f2e
adt315 <angela_dyan@hotmail.com>
2021-01-09 19:09:35 -0500

    minor changes to fix errors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 
-const highscores = require("./../models/memorygame.js");
+const highscores = require("./models/memorygame");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.post("/submit", ({ body }, res) => {
   User.create(body)
