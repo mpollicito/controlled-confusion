492f8c25213df72d28bb23ac91c5aab514838cf0
SusanBeno <beno.susan@gmail.com>
2021-01-12 18:30:09 -0500

    updating get request

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,7 +2,8 @@ const db = require("../models");
 
 module.exports = {
     listAll: function(req, res) {
-        db.User
+        console.log("get call")
+        db.Highscore
         .find(req.body)
         .sort({ score: -1 })
         .then(scoreList => res.json(scoreList))
@@ -10,14 +11,16 @@ module.exports = {
     },
 
     create: function(req, res) {
-        db.User
+        console.log("create call")
+        db.Highscore
         .create(req.body)
         .then(scoreList => res.json(scoreList))
         .catch(err => res.status(422).json(err));
     },
 
     update: function(req, res) {
-        db.User
+        console.log("update call")
+        db.Highscore
         .newList(req.body)
         .then(scoreList => res.json(scoreList))
         .catch(err => res.status(422).json(err));
