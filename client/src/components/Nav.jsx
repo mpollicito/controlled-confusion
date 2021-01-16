28f7eeaedcfd3a375b8bf049a9f86bcc14dad5ea
adt315 <angela_dyan@hotmail.com>
2021-01-15 21:55:27 -0500

    added images, folder, added more card image examples, adjusted example card sizes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,9 +4,9 @@ function Nav() {
   return (
 
     <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo">React Memory Game</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">React Memory Game</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="sass.html">Home</a></li>
           <li><a href="badges.html">Score</a></li>
           <li><a href="collapsible.html">About</a></li>
