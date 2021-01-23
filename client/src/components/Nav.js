db66cd9a993e7e0b9e7e61f5484552e8590b6f9f
adt315 <angela_dyan@hotmail.com>
2021-01-22 20:36:30 -0500

    add pages and nav links

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,17 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-
     <nav>
-
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">Game Set Match</a>
+        <Link className="brand-logo" to="/">
+          Game Set Match
+        </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="sass.html">Home</a></li>
-          <li><a href="badges.html">Scores</a></li>
-          <li><a href="collapsible.html">About</a></li>
-
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/" ||
+                window.location.pathname === "/home"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/scores"
+              className={
+                window.location.pathname === "/portfolio"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Scores
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className={
+                window.location.pathname === "/contact"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Nav from "../components/Nav";
+import Gameboard from "../components/Gameboard";
+
+
+function About() {
+    return (
+    <div>
+    <Gameboard />
+  
+    </div>
+    );
+  }
+  
+  export default About;
+
+
